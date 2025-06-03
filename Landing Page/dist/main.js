143fda401f756eeecd69c08fad70a5b69f7a1b48
ikethedev <isaacdpresley@gmail.com>
2025-06-02 22:06:23 -0400

    mobile menu toggle icon animated

--- Landing Page/dist/main.js
+++ Landing Page/dist/main.js
@@ -1,2 +1,10 @@
 "use strict";
 console.log("Hello, TypeScript!");
+// show menu
+const hamburgerMenu = document.querySelector("#hamburger");
+
+const toggleMenu = () => {
+    document.querySelector("#hamburger").classList.toggle("open");
+};
+
+hamburgerMenu === null || hamburgerMenu === void 0 ? void 0 : hamburgerMenu.addEventListener("click", toggleMenu);
--- Landing Page/src/main.ts
+++ Landing Page/src/main.ts
@@ -1,1 +1,12 @@
 console.log("Hello, TypeScript!");
+// show menu
+const hamburgerMenu = document.querySelector("#hamburger")
+
+const toggleMenu = () => {
+   const menuItems = document.querySelectorAll(".hamburger__line")
+   menuItems.forEach(item => item.classList.toggle("open"))
+
+}
+
+hamburgerMenu?.addEventListener("click", toggleMenu)
+
